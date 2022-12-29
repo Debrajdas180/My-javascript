a3e3daec35d152904b77a33057df28659521d7ef
Debraj Das <80595897+Debrajdas180@users.noreply.github.com>
2022-12-29 23:25:57 +0530

    first commit

---
+++ example.js
@@ -0,0 +1,22 @@
+var a = 78; //variable for integer values
+var b = Debraj; //variable for string
+console.log(a); //for printing the value of a var
+
+/* Operators in javaScript
+In 3 + 4 '+' is the operator and 3,4 are operands 
+1. unary operators - It has single operand (x = -x)
+2. binary operators - It has two oprand (x = x+6) */ 
+   
+// oprand - entities on which operators operate  
+var num1 = 6;
+var num2 = 5;
+console.log("The value of num1 + num2 = " + (num1 + num2)); // Addition 
+console.log("The value of num1 - num2 = " + (num1 - num2)); // Subtraction
+console.log("The value of num1 * num2 = " + (num1 * num2)); // Multiplication
+console.log("The value of num1 / num2 = " + (num1 / num2)); // Division
+console.log("The value of num1 ** num2 = " + (num1 ** num2)); // Exponential operators
+console.log("The value of num1 ++ = " + (num1 ++)); // after printing the value of num1 it will be increment // Increment operator
+console.log("The value of ++ num1  = " + (++ num1 )); // after increment of the value of num1 it will be print // Increment operator
+console.log("The value of num1 -- = " + (num1 --)); // after printing the value of num1 it will be decrement // Decrement operator
+console.log("The value of -- num1  = " + (-- num1 )); // after decrement of the value of num1 it will be print // Decrement operator
+// example of Arithmetic operators
