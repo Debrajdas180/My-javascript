fbbd4ff6af5109fbb500d5df8e8940da2b4e7833
Debraj Das <80595897+Debrajdas180@users.noreply.github.com>
2022-12-30 18:01:44 +0530

    updated

--- example.js
+++ example.js
@@ -1,22 +1,35 @@
 var a = 78; //variable for integer values
-var b = Debraj; //variable for string
+var b = "Debraj"; //variable for stringvr
 console.log(a); //for printing the value of a var
 
 /* Operators in javaScript
 In 3 + 4 '+' is the operator and 3,4 are operands 
 1. unary operators - It has single operand (x = -x)
-2. binary operators - It has two oprand (x = x+6) */ 
-   
-// oprand - entities on which operators operate  
+2. binary operators - It has two oprand (x = x+6) */
+
+// oprand - entities on which operators operate
 var num1 = 6;
 var num2 = 5;
-console.log("The value of num1 + num2 = " + (num1 + num2)); // Addition 
+console.log("The value of num1 + num2 = " + (num1 + num2)); // Addition
 console.log("The value of num1 - num2 = " + (num1 - num2)); // Subtraction
-console.log("The value of num1 * num2 = " + (num1 * num2)); // Multiplication
-console.log("The value of num1 / num2 = " + (num1 / num2)); // Division
-console.log("The value of num1 ** num2 = " + (num1 ** num2)); // Exponential operators
-console.log("The value of num1 ++ = " + (num1 ++)); // after printing the value of num1 it will be increment // Increment operator
-console.log("The value of ++ num1  = " + (++ num1 )); // after increment of the value of num1 it will be print // Increment operator
-console.log("The value of num1 -- = " + (num1 --)); // after printing the value of num1 it will be decrement // Decrement operator
-console.log("The value of -- num1  = " + (-- num1 )); // after decrement of the value of num1 it will be print // Decrement operator
+console.log("The value of num1 * num2 = " + num1 * num2); // Multiplication
+console.log("The value of num1 / num2 = " + num1 / num2); // Division
+console.log("The value of num1 ** num2 = " + num1 ** num2); // Exponential operators
+console.log("The value of num1 ++ = " + num1++); // after printing the value of num1 it will be increment // Increment operator
+console.log("The value of ++ num1  = " + ++num1); // after increment of the value of num1 it will be print // Increment operator
+console.log("The value of num1 -- = " + num1--); // after printing the value of num1 it will be decrement // Decrement operator
+console.log("The value of -- num1  = " + --num1); // after decrement of the value of num1 it will be print // Decrement operator
 // example of Arithmetic operators
+
+var name = "Debraj";
+var char = "and soul";
+var temp = `${name} is a nice person and he has good heart ${char}`;//for attaching the var data in the string
+console.log(temp);
+var len = name.length;//for know the value of the string
+console.log(`The length of the name var is ${len}`);
+console.log("Debraj is a good boy \n Debraj"); //escape sequence character for new line 
+console.log("Debraj is a good boy \t Debraj"); //escape sequence character for horizontal tab 
+console.log("Debraj is a good boy \r Debraj"); //escape sequence character for carriage return 
+console.log("Debraj is a good boy \f Debraj"); //escape sequence character for form feed (new page)
+console.log("Debraj is a good boy \b Debraj"); //escape sequence character for backspace
+
